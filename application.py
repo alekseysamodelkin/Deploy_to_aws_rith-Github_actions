d3804e17dcafaae6396ca8c39bfbb2d4169ac7e8
Aleksey Samodelkin <Samodelkin.lesha@mail.ru>
2020-12-08 14:09:23 +0000

    HW

--- application.py
+++ application.py
@@ -106,7 +106,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations, Aleksey Samodelkin!!!</h1>
+    <h1>Hello world from Samodelkin</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
